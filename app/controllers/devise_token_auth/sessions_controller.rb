573f5e34c06cca9db16324a5fdcb3103e1c94dac
Dave Rhinehart <drhinehart@gmail.com>
2015-12-21 17:30:10 -0600

    allowing login by username or email

--- app/controllers/devise_token_auth/sessions_controller.rb
+++ app/controllers/devise_token_auth/sessions_controller.rb
@@ -5,17 +5,37 @@ module DeviseTokenAuth
 
     def create
       # Check
-      field = (resource_params.keys.map(&:to_sym) & resource_class.authentication_keys).first
+      #field = (resource_params.keys.map(&:to_sym) & resource_class.authentication_keys).first
 
       @resource = nil
-      if field
-        q_value = resource_params[field]
+      # if field
+      #   q_value = resource_params[field]
 
-        if resource_class.case_insensitive_keys.include?(field)
-          q_value.downcase!
-        end
+      #   if resource_class.case_insensitive_keys.include?(field)
+      #     q_value.downcase!
+      #   end
 
-        q = "#{field.to_s} = ? AND provider='email'"
+      #   q = "#{field.to_s} = ? AND provider='email'"
+
+      #   if ActiveRecord::Base.connection.adapter_name.downcase.starts_with? 'mysql'
+      #     q = "BINARY " + q
+      #   end
+
+      #   @resource = resource_class.where(q, q_value).first
+      # end
+
+      q_value = resource_params[:username]
+
+      q = "username = ? AND provider='email'"
+
+      if ActiveRecord::Base.connection.adapter_name.downcase.starts_with? 'mysql'
+        q = "BINARY " + q
+      end
+
+      @resource = resource_class.where(q, q_value).first
+
+      if !@resource
+        q = "email = ? AND provider='email'"
 
         if ActiveRecord::Base.connection.adapter_name.downcase.starts_with? 'mysql'
           q = "BINARY " + q
@@ -24,7 +44,7 @@ module DeviseTokenAuth
         @resource = resource_class.where(q, q_value).first
       end
 
-      if @resource and valid_params?(field, q_value) and @resource.valid_password?(resource_params[:password]) and @resource.confirmed?
+      if @resource and valid_params?(:username, q_value) and @resource.valid_password?(resource_params[:password]) and @resource.confirmed?
         # create client id
         @client_id = SecureRandom.urlsafe_base64(nil, false)
         @token     = SecureRandom.urlsafe_base64(nil, false)
